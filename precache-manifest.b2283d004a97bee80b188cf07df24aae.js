cbdbdc84b590d9e3c00478d448c99b2f146dad7f
Rodrigo Novaes <rod_novaes@hotmail.com>
2020-08-21 12:33:08 +1200

    Updates

---
+++ precache-manifest.b2283d004a97bee80b188cf07df24aae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bec6d1dd3120b764e3049b7ba9084bc4",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "14c9adef900be4267cf3",
+    "url": "/Monsters-Rolodex/static/css/main.6ff01b9b.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/Monsters-Rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14c9adef900be4267cf3",
+    "url": "/Monsters-Rolodex/static/js/main.8004dbb6.chunk.js"
+  },
+  {
+    "revision": "f79345753b7bd810d938",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.a9daa82d.js"
+  }
+]);
